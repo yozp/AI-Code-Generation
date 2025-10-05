03e800ac983522d3a4cb0e012e8f03a0fa84da41
yozp <a1371361879@outlook.com>
2025-10-05 10:19:49 +0800

    项目初始化

---
+++ src/main/java/com/yzj/aicodegeneration/AiCodeGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.yzj.aicodegeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodeGenerationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiCodeGenerationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yzj/aicodegeneration/AiCodeGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yzj.aicodegeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodeGenerationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
