365809b9389f7977c1d8f40838a65f3e931c1cbe
yozp <a1371361879@outlook.com>
2025-10-06 00:43:01 +0800

    用户模块

--- src/main/java/com/yzj/aicodegeneration/AiCodeGenerationApplication.java
+++ src/main/java/com/yzj/aicodegeneration/AiCodeGenerationApplication.java
@@ -1,11 +1,13 @@
 package com.yzj.aicodegeneration;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.yzj.aicodegeneration.mapper")
 public class AiCodeGenerationApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/yzj/aicodegeneration/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.yzj.aicodegeneration.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.yzj.aicodegeneration.model.entity.User;
+
+/**
+ *  映射层。
+ *
+ * @author <a href="https://github.com/yozp">yunikon</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
