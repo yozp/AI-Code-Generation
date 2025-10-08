64e66fa232f669537e6803c1c9a4bee5c8ce8071
yozp <a1371361879@outlook.com>
2025-10-08 12:07:27 +0800

    SSE流式输出

--- src/main/java/com/yzj/aicodegeneration/ai/AiCodeGeneratorService.java
+++ src/main/java/com/yzj/aicodegeneration/ai/AiCodeGeneratorService.java
@@ -3,6 +3,7 @@ package com.yzj.aicodegeneration.ai;
 import com.yzj.aicodegeneration.ai.model.HtmlCodeResult;
 import com.yzj.aicodegeneration.ai.model.MultiFileCodeResult;
 import dev.langchain4j.service.SystemMessage;
+import reactor.core.publisher.Flux;
 
 /**
  * LangChain4j AI 服务接口
@@ -29,5 +30,24 @@ public interface AiCodeGeneratorService {
     @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
     MultiFileCodeResult generateMultiFileCode(String userMessage);
 
+    //-----------------------------------------------------------------------------------------
+
+    /**
+     * 生成 HTML 代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    Flux<String> generateHtmlCodeStream(String userMessage);
+
+    /**
+     * 生成多文件代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    Flux<String> generateMultiFileCodeStream(String userMessage);
 
 }
